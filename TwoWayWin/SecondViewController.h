41686a02e5de37fa423e83e52b7f202b98e5c60c
Troy Jaeger <troy@Troys-Mac-mini.local>
2014-03-04 20:48:30 -0800

    Initial Commit

---
+++ TwoWayWin/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TwoWayWin
+//
+//  Created by Troy Jaeger on 3/4/14.
+//  Copyright (c) 2014 Troy Jaeger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
